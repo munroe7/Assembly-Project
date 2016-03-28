42ae74fd9028aa5c564649729185c61c11ed0432
Samuel Munroe <munroes@wit.edu>
2016-03-28 14:37:33 -0400

    Update main.asm
    Added description

--- main.asm
+++ main.asm
@@ -1,3 +1,9 @@
+;Made by Sam Munroe and Kevin Caulfield
+;Class: Assembly Language
+;Professor: John Russo
+;Date: 12/10/14
+;Purpose: We made a memory game in Assembly. This is based off the generic "memory card flip game".
+
 INCLUDE Irvine32.inc
 .data
 
